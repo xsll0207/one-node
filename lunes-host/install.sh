c74abf6d5ed416396aa3435f7d212dbaaa4f60a1
vevc <dahaimaomi@gmail.com>
2025-08-26 04:45:21 +0000

    feat: add lunes-host module

---
+++ lunes-host/install.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env sh
+
+DOMAIN="${DOMAIN:-node68.lunes.host}"
+PORT="${PORT:-10008}"
+UUID="${UUID:-2584b733-9095-4bec-a7d5-62b473540f7a}"
+HY2_PASSWORD="${HY2_PASSWORD:-vevc.HY2.Password}"
+
+curl -sSL -o config.json https://raw.githubusercontent.com/vevc/one-node/refs/heads/dev/lunes-host/app.js
+curl -sSL -o config.json https://raw.githubusercontent.com/vevc/one-node/refs/heads/dev/lunes-host/package.json
+
+mkdir -p /home/container/xy
+cd /home/container/xy
+curl -sSL -o Xray-linux-64.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+unzip Xray-linux-64.zip
+rm -f Xray-linux-64.zip
+curl -sSL -o config.json https://raw.githubusercontent.com/vevc/one-node/refs/heads/dev/lunes-host/xray-config.json
+sed -i "s/10008/$PORT/g" config.json
+sed -i "s/YOUR_UUID/$UUID/g" config.json
+keyPair=$(./xy x25519)
+privateKey=$(echo "$keyPair" | grep "Private key" | awk '{print $3}')
+publicKey=$(echo "$keyPair" | grep "Public key" | awk '{print $3}')
+sed -i "s/YOUR_PRIVATE_KEY/$privateKey/g" config.json
+shortId=$(openssl rand -hex 4)
+sed -i "s/YOUR_SHORT_ID/$shortId/g" config.json
+vlessUrl="vless://$UUID@$DOMAIN:$PORT?encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.cloudflare.com&fp=chrome&pbk=$publicKey&sid=$shortId&spx=%2F&type=tcp&headerType=none#lunes-reality"
+echo vlessUrl > /home/container/node.txt
+
+mkdir -p /home/container/h2
+cd /home/container/h2
+curl -sSL -o h2 https://github.com/apernet/hysteria/releases/latest/download/hysteria-linux-amd64
+curl -sSL -o config.yaml https://raw.githubusercontent.com/vevc/one-node/refs/heads/dev/lunes-host/hysteria-config.yaml
+openssl req -x509 -newkey rsa:2048 -days 3650 -nodes -keyout key.pem -out cert.pem -subj "/CN=v.com"
+chmod +x h2
+sed -i "s/10008/$PORT/g" config.yaml
+sed -i "s/HY2_PASSWORD/$HY2_PASSWORD/g" config.yaml
+hy2Url="hysteria2://$HY2_PASSWORD@$DOMAIN:$PORT?insecure=1#lunes-hy2"
+echo hy2Url >> /home/container/node.txt
+
+echo "============================================================"
+echo "🚀 VLESS Reality & HY2 Node Info"
+echo "------------------------------------------------------------"
+echo "$vlessUrl"
+echo "$hy2Url"
+echo "============================================================"
